d75c648a60484216f606df2f5b7b6082c8609471
Peter Vandenberk <peter.vandenberk@simplybusiness.co.uk>
2020-01-07 15:06:25 +0000

    gemstash versioning for dependabot

--- lib/packer/version.rb
+++ lib/packer/version.rb
@@ -1,5 +1,9 @@
 # frozen_string_literal: true
 
 module Packer
-  VERSION = '0.7.0'
+  VERSION = '0.7.0' # rubocop:disable Style/MutableConstant
+
+  # SB-specific versioning "algorithm" to accommodate BNW/Jenkins/gemstash
+  VERSION << '.' << ENV['GEM_PRE_RELEASE'].strip \
+  unless ENV.fetch('GEM_PRE_RELEASE', '').strip.empty?
 end
