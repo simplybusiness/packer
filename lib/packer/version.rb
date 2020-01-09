12a7c2780735f8fb9c3ab2b9e83d3fdefccd726b
Peter Vandenberk <peter.vandenberk@simplybusiness.co.uk>
2020-01-09 13:38:31 +0000

    bump version for new gemstash release

--- lib/packer/version.rb
+++ lib/packer/version.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 module Packer
-  base = '0.7.0'
+  base = '0.7.1'
 
   # SB-specific versioning "algorithm" to accommodate BNW/Jenkins/gemstash
   VERSION = (pre = ENV.fetch('GEM_PRE_RELEASE', '')).empty? ? base : "#{base}.#{pre}"
