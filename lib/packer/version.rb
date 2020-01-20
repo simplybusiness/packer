0050ed2998fa27e89c3b6b6c04c457e4b4bca0f4
Robert Hall <robert.hall@simplybusiness.co.uk>
2020-01-20 14:27:29 +0000

    bump version

--- lib/packer/version.rb
+++ lib/packer/version.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 module Packer
-  base = '0.7.2'
+  base = '0.7.3'
 
   # SB-specific versioning "algorithm" to accommodate BNW/Jenkins/gemstash
   VERSION = (pre = ENV.fetch('GEM_PRE_RELEASE', '')).empty? ? base : "#{base}.#{pre}"
